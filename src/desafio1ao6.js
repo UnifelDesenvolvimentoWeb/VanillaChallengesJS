9a1a6b0f3fa6d894bfed6128b3205ffa3d8ff998
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-21 16:10:10 -0300

    Desafio 03
    Concluido

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -1,5 +1,5 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+const arrayProp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const arrayDupli = [3, 5, 5, 7, 9, 9, 1, 9, 2, 5, 7, 9, 2, 42, 12, 56, 87, 34, 23, 34]
 
 
@@ -23,10 +23,16 @@ biggerNumber(array)
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
-function ignoreFirstAndLastElement(array) {
-  
+function ignoreFirstAndLastElement(arrayProp) {
+  if (arrayProp.length <= 3) {
+    console.log('Tamanho do array inválido')
+  } else {
+    let arrayUltimoIndex = arrayProp.pop()
+    let arrayPrimeiroIndex = arrayProp.shift()
+    console.log(arrayProp)
+  }
 }
-ignoreFirstAndLastElement(array)
+ignoreFirstAndLastElement(arrayProp)
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
