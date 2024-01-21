30d4a3222980b98dd4e55732918244a0973d6236
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-21 15:46:29 -0300

    Desafio 06
    Concluido

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -1,5 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const arrayDupli = [3, 5, 5, 7, 9, 9, 1, 9, 2, 5, 7, 9, 2, 42, 12, 56, 87, 34, 23, 34]
+
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
@@ -22,8 +24,9 @@ biggerNumber(array)
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
-
+  
 }
+ignoreFirstAndLastElement(array)
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
@@ -35,16 +38,20 @@ invertElementsArray(array)
 
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
-function invertString(string) {
 
+function invertString(string) {
+   
 }
 
 // Desafio 6
 // Escreva um código que receba um array de números por parâmetro e retorne
 // o array com os elementos duplicados removidos.
-function noDuplicates(array) {
-  
+function noDuplicates(arrayDupli) {
+  const set = new Set(arrayDupli);
+  const arraySemDuplicados = Array.from(set);
+  console.log(arraySemDuplicados); 
 }
+noDuplicates(arrayDupli)
 
 module.exports = {
   biggerNumber,
