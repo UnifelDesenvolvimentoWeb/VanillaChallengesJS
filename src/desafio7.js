5d07e842cd54d1abd2b78c975daed5992fd7f787
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-22 19:21:43 -0300

    Testando soluções

--- src/desafio7.js
+++ src/desafio7.js
@@ -1,7 +1,13 @@
 // Desafio 7
+
 function taxesCalcul(grossSalary) {
- 
+    if (grossSalary <= Number("1.556,94")) {
+        return "podre"
+    }
+    
 }
+console.log(taxesCalcul(200))
+
 
 module.exports = {
     taxesCalcul
--- src/desafio8.js
+++ src/desafio8.js
@@ -1,8 +1,14 @@
 // Desafio 8
+const telNumber = [0, 7, 1, 9, 9, 1, 5, 0, 4, 8, 3, 4]
 function formatPhoneNumber(telNumber) {
+    if ( telNumber.length <= 11) {
+        console.log('Array com tamanho incorreto.')
+    } else if ( )
+    //let cel = telNumber.join("")
+    //console.log(cel)
     
 }
-
+formatPhoneNumber(telNumber)
 module.exports = {
     formatPhoneNumber
 }
