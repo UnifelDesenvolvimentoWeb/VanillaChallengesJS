4465bebdee381112780aaaf52b6d82ae71639fd5
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-23 14:10:22 -0300

    Atualização Desafio 08

--- src/desafio8.js
+++ src/desafio8.js
@@ -1,11 +1,10 @@
 // Desafio 8
 const telNumber = [0, 7, 1, 9, 9, 1, 5, 0, 4, 8, 3, 4]
 function formatPhoneNumber(telNumber) {
-    if ( telNumber.length <= 11) {
+    let cel = telNumber.join("")
+    if ( cel.length <= 11) {
         console.log('Array com tamanho incorreto.')
-    } else if ( )
-    //let cel = telNumber.join("")
-    //console.log(cel)
+    } else if ()
     
 }
 formatPhoneNumber(telNumber)
