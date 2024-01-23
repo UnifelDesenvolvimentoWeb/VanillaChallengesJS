e58ff77fb8f3dd727f36ab6783ad464999017bd2
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-23 14:44:25 -0300

    Atualização Desafio 08
    Buscando a resolução do Else

--- src/desafio8.js
+++ src/desafio8.js
@@ -4,7 +4,13 @@ function formatPhoneNumber(telNumber) {
     let cel = telNumber.join("")
     if ( cel.length <= 11) {
         console.log('Array com tamanho incorreto.')
-    } else if ()
+    } else if (telNumber.some(it => it < 0)) {
+        console.log('não é possível gerar um número de telefone com esses valores')
+    } else if (telNumber.some(it => it > 10)) {
+        console.log('não é possível gerar um número de telefone com esses valores')
+    } else {
+        console.log(cel)
+    }
     
 }
 formatPhoneNumber(telNumber)
