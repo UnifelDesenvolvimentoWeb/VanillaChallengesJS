196ab5a9c9296df40a84bf250bab98f9b5066ce0
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-23 15:19:33 -0300

    Atualização desafio 08
    Desafio concluido

--- src/desafio8.js
+++ src/desafio8.js
@@ -1,15 +1,22 @@
 // Desafio 8
-const telNumber = [0, 7, 1, 9, 9, 1, 5, 0, 4, 8, 3, 4]
+const telNumber = [7, 1, 9, 9, 1, 5, 0, 4, 8, 3, 4]
 function formatPhoneNumber(telNumber) {
     let cel = telNumber.join("")
-    if ( cel.length <= 11) {
+    if ( cel.length <= 10) {
         console.log('Array com tamanho incorreto.')
     } else if (telNumber.some(it => it < 0)) {
         console.log('não é possível gerar um número de telefone com esses valores')
     } else if (telNumber.some(it => it > 10)) {
         console.log('não é possível gerar um número de telefone com esses valores')
     } else {
-        console.log(cel)
+        const phoneMask = (cel) => {
+            if (!cel) return ""
+            cel = cel.replace(/\D/g,'')
+            cel = cel.replace(/(\d{2})(\d)/,"($1) $2")
+            cel = cel.replace(/(\d)(\d{4})$/,"$1-$2")
+            return cel
+        }
+        console.log(phoneMask(cel))
     }
     
 }
