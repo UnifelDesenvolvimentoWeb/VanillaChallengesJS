e75f8eeaa937626a2bbcf683ed80a10b3de8e10d
Laiz <laizsc.89@gmail.com>
2024-01-24 19:50:42 -0300

    resolução do desafio 8

--- src/desafio8.js
+++ src/desafio8.js
@@ -1,7 +1,29 @@
 // Desafio 8
 function formatPhoneNumber(telNumber) {
+ if (telNumber.length !== 11) {
+   return 'Array com tamanho incorreto.';
+ }
+ let contadorOcorrencias =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+ for (let i = 0; i < telNumber.length; i++) {
     
+    if (telNumber[i] < 0 || telNumber[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+    }
+    contadorOcorrencias[telNumber[i]]++;
+    if (contadorOcorrencias[telNumber[i]] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+ }
+ 
+//  for (num of contadorOcorrencias) {
+//   if (num >= 3) {
+//     return 'não é possível gerar um número de telefone com esses valores';
+//   }
+//  }                                 
+
+return `(${telNumber.slice(0, 2).join('')}) ${telNumber.slice(2, 7).join('')}-${telNumber.slice(7).join('')}`;
 }
+
 
 module.exports = {
     formatPhoneNumber
