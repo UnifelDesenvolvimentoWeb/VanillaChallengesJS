b36d6380bbac92530ac5d6cfc71e0534faef64dc
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-24 16:47:37 -0300

    Atualizando a concusão do desafio 09

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,8 +1,9 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
+  return array.slice(inicio, fim)
    
 }
-
+console.log(getArrayNoIntervalo([1, 2, 3, 4, 5, 1], 1, 4))
 module.exports = {
   getArrayNoIntervalo
 }
