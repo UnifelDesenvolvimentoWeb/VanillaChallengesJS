fc165f81b539d5e2fbe92be86dfffb9a184afa0c
[Matheus] <[lastb0402@gmail.com]>
2024-01-24 16:56:01 -0300

    Desafio 1

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,8 +2,10 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-
+function printsElements(array) {
+  for(let i = 0; i < array.length; i += 1) {
+    console.log(array[i]);
+  }
 }
 
 // Desafio 2
@@ -40,7 +42,7 @@ function noDuplicates(array) {
 
 module.exports = {
   biggerNumber,
-  printElements,
+  printsElements,
   ignoreFirstAndLastElement,
   invertElementsArray,
   invertString,
