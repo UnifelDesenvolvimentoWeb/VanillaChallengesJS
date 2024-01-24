917fcf87d27b14d11570080307a8faff6068ade2
mariolemos <lemosmm64@gmail.com>
2024-01-24 08:10:41 -0300

    Primeiro desafio

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,8 +3,11 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-
-}
+	for(let i = 0; i<array.length; i++){
+		console.log(array[i]);
+		}
+	}
+	printElements(array);
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
