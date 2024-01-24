d3260a6e3b264f3da7c7401155fc1f98172497e0
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-24 18:26:58 -0300

    Atualizando a concusão do desafio 1ao6

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -1,7 +1,6 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arrayProp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const name = 'Patrick'
-const arrayDupli = [3, 5, 5, 7, 9, 9, 1, 9, 2, 5, 7, 9, 2, 42, 12, 56, 87, 34, 23, 34]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10];
+const string = 'Patrick'
+
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
@@ -25,16 +24,16 @@ function biggerNumber(array) {
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 
-function ignoreFirstAndLastElement(arrayProp) {
-  if (arrayProp.length <= 3) {
+function ignoreFirstAndLastElement(array) {
+  if (array.length <= 3) {
     console.log('Tamanho do array inválido')
   } else {
-    let arrayUltimoIndex = arrayProp.pop()
-    let arrayPrimeiroIndex = arrayProp.shift()
-    console.log(arrayProp)
+    let arrayUltimoIndex = array.pop()
+    let arrayPrimeiroIndex = array.shift()
+    console.log(array)
   }
 }
-ignoreFirstAndLastElement(arrayProp)
+ignoreFirstAndLastElement(array)
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
@@ -48,7 +47,7 @@ function invertElementsArray(array) {
     array[esquerda] = array[direita]
     array[direita] = temp
   }
-  console.log(array)
+  console.log(array) 
 }
 invertElementsArray(array)
 
@@ -56,22 +55,22 @@ invertElementsArray(array)
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
 
-function invertString(name) {
-  let nameInverter = name.split("").reverse().join("")
+function invertString(string) {
+  let nameInverter = string.split("").reverse().join("")
   console.log(nameInverter)  
 }
-invertString(name)
+invertString(string)
 
 // Desafio 6
 // Escreva um código que receba um array de números por parâmetro e retorne
 // o array com os elementos duplicados removidos.
 
-function noDuplicates(arrayDupli) {
-  const set = new Set(arrayDupli);
+function noDuplicates(array) {
+  const set = new Set(array);
   const arraySemDuplicados = Array.from(set);
   console.log(arraySemDuplicados); 
 }
-noDuplicates(arrayDupli)
+noDuplicates(array)
 
 module.exports = {
   biggerNumber,
