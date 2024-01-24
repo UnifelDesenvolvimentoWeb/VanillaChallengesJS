b147bd6be7d183bdbf473e7c24d1cb85e4e130fa
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-24 12:48:44 -0300

    Desafio 01 - Concluido!

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,8 +3,11 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-
+  for (let index = 0; index < array.length; index++) {
+    console.log(array[index])
+  } 
 }
+printElements(array)
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
