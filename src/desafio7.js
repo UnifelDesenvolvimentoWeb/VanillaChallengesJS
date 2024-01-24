fffbae5e741882767f193a49b2b77b3e05f17c52
Luana Dias <diasluana083@gmail.com>
2024-01-23 21:03:39 -0300

    Calculo do INSS

--- src/desafio7.js
+++ src/desafio7.js
@@ -1,8 +1,19 @@
 // Desafio 7
 function taxesCalcul(grossSalary) {
- 
+ let inss;
+ let ir;
+ if (grossSalary <= 1556.94) {
+    inss = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+    inss = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+    inss = grossSalary * 0.11;
+} else {
+    inss = 570.88
 }
-
+return inss
+}
+console.log(taxesCalcul(5189.82));
 module.exports = {
     taxesCalcul
 }
