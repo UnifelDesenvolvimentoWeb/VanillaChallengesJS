100ba67a572d3da5b17b2ce37bf515ff83fb377e
Wellington Farias <tonw2farias@gmail.com>
2024-01-24 20:49:07 -0300

    Primeiro Desafio Concluido

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,34 +2,50 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-
+function printsElements(array) {
+  
+  for(let i = 0; i< array.length; i+=1){
+    let s = array[i];
+    console.log(s);
+    
+  }
+  
 }
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
+
 function biggerNumber(array) {
-
-}
-
+  
+    
+    
+  }
+  
+  
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
+
+  
 
 }
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
 function invertElementsArray(array) {
-
+ console.log(array.short);
 }
+
+
 
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
 function invertString(string) {
-
+  
 }
+
+
 
 // Desafio 6
 // Escreva um código que receba um array de números por parâmetro e retorne
@@ -40,7 +56,7 @@ function noDuplicates(array) {
 
 module.exports = {
   biggerNumber,
-  printElements,
+  printsElements,
   ignoreFirstAndLastElement,
   invertElementsArray,
   invertString,
