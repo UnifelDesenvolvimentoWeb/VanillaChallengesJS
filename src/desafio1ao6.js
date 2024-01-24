ebcff0f567cdf9b91e5fa4830d6e2aa44c4e45a7
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-24 17:56:22 -0300

    Atualizando a concusão do desafio 1ao6

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,31 +3,28 @@ const arrayProp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const name = 'Patrick'
 const arrayDupli = [3, 5, 5, 7, 9, 9, 1, 9, 2, 5, 7, 9, 2, 42, 12, 56, 87, 34, 23, 34]
 
-
-
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
+
 function printElements(array) {
   for (let index = 0; index < array.length; index++) {
     console.log(array[index])
   } 
 }
-console.log(printElements(array))
+printElements(array)
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
+
 function biggerNumber(array) {
-  maxValue = array.reduce(function(prev, current) {
-    return prev > current ? prev : current  
-  })
-  return maxValue 
   
 }
-biggerNumber(array)
+
 
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
+
 function ignoreFirstAndLastElement(arrayProp) {
   if (arrayProp.length <= 3) {
     console.log('Tamanho do array inválido')
@@ -41,6 +38,7 @@ ignoreFirstAndLastElement(arrayProp)
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
+
 function invertElementsArray(array) {
   let tamanho = array.length
   let esquerda = null
@@ -54,8 +52,10 @@ function invertElementsArray(array) {
 }
 invertElementsArray(array)
 
+
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
+
 function invertString(name) {
   let nameInverter = name.split("").reverse().join("")
   console.log(nameInverter)  
@@ -65,6 +65,7 @@ invertString(name)
 // Desafio 6
 // Escreva um código que receba um array de números por parâmetro e retorne
 // o array com os elementos duplicados removidos.
+
 function noDuplicates(arrayDupli) {
   const set = new Set(arrayDupli);
   const arraySemDuplicados = Array.from(set);
