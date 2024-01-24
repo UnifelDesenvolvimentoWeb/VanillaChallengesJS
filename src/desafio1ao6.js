90cc5b00ee40d296a4be6a272872cb0955dc2992
Francine Verena <francineverena@hotmail.com>
2024-01-24 17:05:08 -0300

    resolvido primeiro desafio

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,18 +2,27 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-  for (let index = 0; index < numeros.length; index++) {
-    console.log(numeros[index]);
+
+  for (let index = 0; index < array.length; index += 1 ) {
+    console.log(array[index]);
 
   }
- 
-}
+
+  function printElements(array) {
+
+  }
+
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
-function biggerNumber(array) {
+
+  maxValue = array(function(prev, current) {
+    return prev > current ? prev : current
+  })
+
+  return maxValue
  
+  function biggerNumber(array) {
 
 }
 
