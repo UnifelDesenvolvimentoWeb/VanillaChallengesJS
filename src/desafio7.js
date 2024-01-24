c56c0d3e57f7e8d5a9c8e15c4e4ce9e20abc5847
Luana Dias <diasluana083@gmail.com>
2024-01-23 21:53:26 -0300

    Desafio 7 feito

--- src/desafio7.js
+++ src/desafio7.js
@@ -1,8 +1,8 @@
 // Desafio 7
 function taxesCalcul(grossSalary) {
- let inss;
- let ir;
- if (grossSalary <= 1556.94) {
+let inss;
+let ir;
+if (grossSalary <= 1556.94) {
     inss = grossSalary * 0.08;
 } else if (grossSalary <= 2594.92) {
     inss = grossSalary * 0.09;
@@ -11,9 +11,25 @@ function taxesCalcul(grossSalary) {
 } else {
     inss = 570.88
 }
-return inss
+
+let baseSalario = grossSalary - inss;
+
+if (baseSalario <= 1903.98){
+    ir = 0;
+} else if (baseSalario <= 2826.65) {
+    ir = (baseSalario * 0.075) - 142.80;
+} else if (baseSalario <= 3751.05) {
+    ir = (baseSalario * 0.15) - 354.80;
+} else if (baseSalario <= 4664.68) {
+    ir = (baseSalario * 0.225) - 636.13
+} else {
+    ir = (baseSalario * 0.275) - 869.36
 }
-console.log(taxesCalcul(5189.82));
+let salarioLiquido = baseSalario - ir
+return `Salário: R$ ${salarioLiquido}`;
+}
+console.log(taxesCalcul(3000));
+
 module.exports = {
     taxesCalcul
 }
