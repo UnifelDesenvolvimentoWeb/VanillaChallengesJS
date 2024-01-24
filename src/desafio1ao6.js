a358e76d846489d23d8cf7190cc3f086149a7e1f
Francine Verena <francineverena@hotmail.com>
2024-01-24 15:20:03 -0300

    desafio

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,13 +3,17 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-  console.log('Desafio 1');
+  for (let index = 0; index < numeros.length; index++) {
+    console.log(numeros[index]);
 
+  }
+ 
 }
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
 function biggerNumber(array) {
+ 
 
 }
 
