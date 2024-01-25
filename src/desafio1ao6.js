8fd35e7c92f9b2064c203ed99fbe9761ddb5270e
AriovaldoSantS <153957898+AriovaldoSantS@users.noreply.github.com>
2024-01-24 21:21:11 -0300

    primeiro Commit

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,33 +2,42 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-
-}
+function printsElements(array) {for (let i = 0; i < array.length; i += 1) {
+  let r = array[i];
+  console.log(r)
+  }}
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
 function biggerNumber(array) {
-
-}
+  let maior = array[0]
+  
+  for (let i = 0; i < array.length; i += 1) 
+  { if (array[i] > maior) {maior = array[i]}
+  }
+  return maior
+  }
 
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
-
+  if (array.length <3) {console.log('Tamanho do array inválido'); return;}
+  for (let i = 1; i < array.length - 1; i++) {console.log(array[i])}
+  
 }
+  
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
-function invertElementsArray(array) {
-
+function invertElementsArray(array){
+  console.log(array.sort((a,b) => b - a))
 }
 
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
-function invertString(string) {
-
+function invertString(str){
+  return str.split("").reverse().join("");
 }
 
 // Desafio 6
@@ -40,7 +49,7 @@ function noDuplicates(array) {
 
 module.exports = {
   biggerNumber,
-  printElements,
+  printsElements,
   ignoreFirstAndLastElement,
   invertElementsArray,
   invertString,
