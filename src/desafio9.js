7b2f0eb5dfd7a8f239c65466b210830917908198
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-01-25 14:44:18 -0300

    Desafio 09 concluido

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,9 +1,32 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
-  return array.slice(inicio, fim)
+  const newArr = array.sort((x,y) => {
+    return x-y;
+  });
+  let res = []
+  let cont = 0
+
+  if (array.length === 0) {
+    return ({"arrayNoIntervalo": [], "contador": 0})
+  } else if(inicio > fim) {
+    return ({"arrayNoIntervalo": [], "contador": 0})
+  } else {
+    for (let index = 0; index < array.length; index += 1) {
+      if (newArr[index] === inicio){
+        res.push(newArr[index]);
+        cont += 1
+      } else if(newArr[index] > inicio & newArr[index] < fim) {
+        res.push(newArr[index]);
+        cont += 1
+      } else if (newArr[index] === fim) {
+        res.push(newArr[index]);
+        cont += 1
+      }
+    }
+    return ({"arrayNoIntervalo": res , "contador": cont})
+  }   
    
 }
-console.log(getArrayNoIntervalo([1, 2, 3, 4, 5, 1], 1, 4))
 module.exports = {
   getArrayNoIntervalo
 }
