4d40f7bd62544be360fe9431a59f2d94b352c608
Yago Passos <yagopassos98@hotmail.com>
2024-01-26 22:12:59 -0300

    desafio 9

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,6 +1,14 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
-   
+  const arrayNoIntervalo = []
+  let contador = 0;
+  for(let value of array){
+    if(value >= inicio && value <= fim){ 
+      arrayNoIntervalo[contador] = value;
+      contador += 1;
+    }
+  }
+  return {arrayNoIntervalo, contador};
 }
 
 module.exports = {
