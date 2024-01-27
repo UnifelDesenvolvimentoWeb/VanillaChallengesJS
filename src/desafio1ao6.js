eef0cf67abc0d0de91e0ebb6bee339057d51e946
ClaudioRobertoSilvaPaz <okdok9@cpaz19.onmicrosoft.com>
2024-01-26 22:11:10 -0300

    Questão 1 - OK

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,16 +2,18 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-
+function printsElements(array) {
+    for(let index = 0; index < array.length; index += 1) {
+     console.log(array[index])
+    }
 }
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
 function biggerNumber(array) {
+  
 
 }
-
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
@@ -40,7 +42,7 @@ function noDuplicates(array) {
 
 module.exports = {
   biggerNumber,
-  printElements,
+  printsElements,
   ignoreFirstAndLastElement,
   invertElementsArray,
   invertString,
