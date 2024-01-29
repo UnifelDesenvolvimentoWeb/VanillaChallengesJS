e75f86d45915fbda46a2a4f5640970e8aa7993cb
@Sowza82 <tathyannesg18@gmail.com>
2024-01-29 14:14:46 -0300

    Desafio 9

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,7 +1,24 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
-   
+  const arrayNoIntervalo = array.filter(
+    (numero) => numero >= inicio && numero <= fim,
+  );
+
+  const contador = arrayNoIntervalo.length;
+
+  return {
+    arrayNoIntervalo: arrayNoIntervalo,
+    contador: contador,
+  };
 }
+
+const arrayOriginal = [14, 18, 27, 39, 68, 81, 93, 115];
+const inicio = 37;
+const fim = 81;
+
+const resultado = getArrayNoIntervalo(arrayOriginal, inicio, fim);
+console.log(resultado);
+
 
 module.exports = {
   getArrayNoIntervalo
