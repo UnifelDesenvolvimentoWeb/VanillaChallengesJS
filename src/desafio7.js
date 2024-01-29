83af12278759d385b485736ea2569aa892761244
@Sowza82 <tathyannesg18@gmail.com>
2024-01-29 13:42:00 -0300

    Desafio 7

--- src/desafio7.js
+++ src/desafio7.js
@@ -1,43 +1,46 @@
 // Desafio 7
 function taxesCalcul(grossSalary) {
     let inss;
-    if (salarioBruto <= 1,556,94){
-        inss = salarioBruto *0.08;
 
- } else if (salarioBruto <= 2594.92) {
-    inss = salarioBruto * 0.09;
- } else if (salarioBruto<=5189.92) {
-    inss =salarioBruto * 0.11;
- }else{
-    inss =570.88;
- }
+  if (grossSalary <= 1556.94) {
+    inss = grossSalary * 0.08;
+  } else if (grossSalary <= 2594.92) {
+    inss = grossSalary * 0.09;
+  } else if (grossSalary <= 5189.92) {
+    inss = grossSalary * 0.11;
+  } else {
+    inss = 570.88;
+  }
 
- const salarioBase =salarioBruto -inss;
+  const salarioBase = grossSalary - inss;
 
- return {
-    salarioBruto:
-    salarioBruto.toFixed(2),
-    inss:inss.toFixed(2),
-    salarioBase : 
-    salarioBase.toFixed(2),
-    ir:ir.toFixed(2),
-    salarioLiquido:
-   salarioLiquido.toFixed(2),
- };
+  const ir = calcularIR(salarioBase);
 
+  const salarioLiquido = salarioBase - ir;
+
+  return {
+    salarioBruto: grossSalary.toFixed(2),
+    inss: inss.toFixed(2),
+    salarioBase: salarioBase.toFixed(2),
+    ir: ir.toFixed(2),
+    salarioLiquido: salarioLiquido.toFixed(2),
+  };
 }
 
-const salarioBrutoExemplo =3000.0;
-const resultado =
-texesCalcul(salarioBrutoExemplo);
+function calcularIR(salarioBase) {
+  
+  return salarioBase * 0.15;
+}
+
+const salarioBrutoExemplo = 3000.0;
+const resultado = taxesCalcul(salarioBrutoExemplo);
 
 console.log("Resultado:");
-console.log("Salario Bruto:", resultado.salarioBase);
-console.log("IR:" , resultado.ir);
-console.log("Salário Líquido:" ,
-resultado.salarioLiquido);
-       
-
+console.log("Salario Bruto:", resultado.salarioBruto);
+console.log("INSS:", resultado.inss);
+console.log("Salário Base:", resultado.salarioBase);
+console.log("IR:", resultado.ir);
+console.log("Salário Líquido:", resultado.salarioLiquido);
 
 module.exports = {
 taxesCalcul
