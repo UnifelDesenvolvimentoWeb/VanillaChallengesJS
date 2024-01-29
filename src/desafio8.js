d96a2dc52d7eee80bb71bf3d097a907daca314a9
AriovaldoSantS <153957898+AriovaldoSantS@users.noreply.github.com>
2024-01-28 23:19:49 -0300

    desafio 8

--- src/desafio8.js
+++ src/desafio8.js
@@ -1,7 +1,30 @@
 // Desafio 8
 function formatPhoneNumber(telNumber) {
+    if(telNumber.length !==11) {
+        return 'Array com tamanho incorreto.';
+    }
     
-}
+    const contNumber = {};
+    for(let i = 0; i < telNumber.length; i++) {
+        const numero = telNumber[i];
+        if(numero < 0 || numero > 9) {
+            return 'não é possível gerar um número de telefone com esses valores';
+        }
+        contNumber[numero] = (contNumber[numero] || 0) + 1;
+        if(contNumber[numero] >= 3) {
+            return 'não é possível gerar um número de telefone com esses valores';
+            } 
+    }
+    const formatoNumero = `(${telNumber[0]}${telNumber[1]}) ${telNumber[2]}${telNumber[3]}${telNumber[4]}${telNumber[5]}${telNumber[6]}-${telNumber[7]}${telNumber[8]}${telNumber[9]}${telNumber[10]}`;
+    return formatoNumero
+    }
+
+    
+   
+    
+    
+    
+    
 
 module.exports = {
     formatPhoneNumber
