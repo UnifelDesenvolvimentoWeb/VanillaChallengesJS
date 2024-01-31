1d440d92fd4745e45c254ae39f8df26771d0220d
Fausto da Cruz <otsuaf1701@gmail.com>
2024-01-31 15:51:55 -0300

    Challenges 1 to 6

--- src/desafio7.js
+++ src/desafio7.js
@@ -1,6 +1,18 @@
 // Desafio 7
 function taxesCalcul(grossSalary) {
- 
+    if(grossSalary < 1556.94)
+        grossSalary -= (grossSalary - ((8/100) * grossSalary))
+
+    else if(grossSalary < 2594.92)
+        grossSalary -= (grossSalary - ((8/100) * grossSalary))
+
+    else if(grossSalary < 5189.82)
+        grossSalary -= (grossSalary - ((8/100) * grossSalary))
+
+    else
+        grossSalary -= 570.88
+    
+    
 }
 
 module.exports = {
