aa88606e12a85d66279f996dd22d37c9a7f2f500
maila520 <mailasantos520@gmail.com>
2024-02-05 11:30:56 -0300

    desafio1

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,14 +2,18 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
-function printElements(array) {
-
+function printElements(array) { 
+   
 }
+for ( let index = 0 ; index<= 10 ; index +=1){
+  console.log (`numero de repetição: ${index}` );
+}
+
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
 function biggerNumber(array) {
-   .
+
 }
 
 // Desafio 3
