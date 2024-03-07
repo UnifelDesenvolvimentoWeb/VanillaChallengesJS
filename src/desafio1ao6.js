be47d7ba9d0dc0e40c3abb457a557d89dbc20c9c
Marcley <marcleyrosa@gmail.com>
2024-03-07 14:36:34 -0300

    primeiro desafio

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,8 +3,10 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-
+  const x = 1
 }
+
+console.log(printElements(array));
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
