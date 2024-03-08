19f8a0ce654efe327641e9dc7858a50c36ee0b70
GabrielBro <gabriel.fsans@hotmail.com>
2024-03-08 12:28:50 -0300

    Desafio 9

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,6 +1,18 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
-   
+
+  let arrayInfo = {
+    arrayNoIntervalo: [],
+    contador: 0
+};
+
+  arrayInfo.arrayNoIntervalo = array.filter(e => {
+      if(e >= inicio && e <= fim) {return e};
+  });
+  
+  arrayInfo.contador = arrayInfo.arrayNoIntervalo.length;
+  
+  return arrayInfo;
 }
 
 module.exports = {
