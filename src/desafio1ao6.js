2b72e4db04060bc675032b03bbd3cd96283c071a
Helena Estrela <estrelahelena7@gmail.com>
2024-03-11 17:12:04 -0300

    Desafios 1 e 2 - update

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -2,9 +2,11 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
+let elements = 0
 function printElements(array) {
   for (let index = 0; index > array.lenght; index += 1) {
-    console.log(array[index])
+    elements += array[index]
+    console.log(elements)
   }
 }
 
