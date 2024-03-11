f376187bf98fcd3e57eff6f0bc09eb0b8c7449ab
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-11 16:10:08 -0300

    Desafio 9 completo

--- src/desafio9.js
+++ src/desafio9.js
@@ -1,6 +1,17 @@
 // Desafio 9
 function getArrayNoIntervalo(array, inicio, fim) {
-   
+  let resultado = {}
+  let contador = 0
+  let array2 = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= inicio && array[i] <= fim) {
+      array2.push(array[i])
+      contador += 1
+    }
+  }
+  resultado.arrayNoIntervalo = array2
+  resultado.contador = contador
+  return resultado
 }
 
 module.exports = {
