f94d3d212d29b38e7aab4e921e4fbe630ceed61a
Ivonete <161178184+Spivonete@users.noreply.github.com>
2024-03-15 00:24:45 -0300

    Desafio 6

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,8 +3,10 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-  console.log (array)
+  return console.log (array)
 }
+printElements(array)
+
 
 // Desafio 2
 // Escreva um código que encontre o maior elemento de um array.
@@ -18,36 +20,39 @@ function biggerNumber(array) {
  		  ma = me
  }
  }
-
-  console.log (array)
-  console.log ( ma )
-
+  return console.log ( ma )
 }
+biggerNumber(array)
+
 
 // Desafio 3
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
   if ( array.length < 3 ) {
-    console.log ("Tamanho do array inválido");
+    return console.log ("Tamanho do array inválido");
   } 
 else {
    for ( let index = 1 ; index < array.length - 1 ; index += 1){
-         console.log(array[index])
+         return console.log(array[index])
    }
      }
 
 }
+ignoreFirstAndLastElement(array)
+
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
 function invertElementsArray(array) {
+  const arrayinvert = []
   for ( let index = 0 ; index < array.length ; index += 1) {
     arrayinvert.unshift ( array[index])
 }
-console.log(arrayinvert)
-
+return console.log(arrayinvert)
 }
+invertElementsArray(array)
+
 
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
@@ -59,8 +64,21 @@ function invertString(string) {
 // Escreva um código que receba um array de números por parâmetro e retorne
 // o array com os elementos duplicados removidos.
 function noDuplicates(array) {
+     const duplicado = array.slice()
+     duplicado.sort()
+     let indice = 0
+     for ( let index = 0 ; index < array.length ; index += 1 ){
+         indice += 1
+         if ( array[index] === duplicado[indice] ){
+             array.splice(index,1)
+         }
+     }
+     return array
+ }
+
+ noDuplicates(array)
   
-}
+
 
 module.exports = {
   biggerNumber,
